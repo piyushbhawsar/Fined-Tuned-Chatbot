c27b0f0a7c15d24be091f1f51d47daa340b7f1f4
piyushbhawsar <111758968+piyushbhawsar@users.noreply.github.com>
2023-05-20 10:50:28 +0530

    fefe

--- index.js
+++ index.js
@@ -1,58 +1,3 @@
-// import { Configuration, OpenAIApi } from 'openai'
-// import { process } from './env'
-
-// const configuration = new Configuration({
-//     apiKey: process.env.OPENAI_API_KEY,
-// })
-
-// const openai = new OpenAIApi(configuration)
-
-// const chatbotConversation = document.getElementById('chatbot-conversation')
- 
-// let conversationStr = ''
- 
-// document.addEventListener('submit', (e) => {
-//     e.preventDefault()
-//     const userInput = document.getElementById('user-input') 
-//     conversationStr += ` ${userInput.value} ->`
-//     fetchReply()
-//     const newSpeechBubble = document.createElement('div')
-//     newSpeechBubble.classList.add('speech', 'speech-human')
-//     chatbotConversation.appendChild(newSpeechBubble)
-//     newSpeechBubble.textContent = userInput.value
-//     userInput.value = ''
-//     chatbotConversation.scrollTop = chatbotConversation.scrollHeight
-// }) 
-
-// async function fetchReply(){
-//     const response = await openai.createCompletion({
-//         model: 'davinci:ft-personal-2023-05-20-04-19-13',
-//         prompt: conversationStr,
-//         presence_penalty: 0,
-//         frequency_penalty: 0.3,
-//         max_tokens: 100,
-//         temperature: 0,
-//         stop: ['\n', '->']
-//     })
-//     conversationStr += ` ${response.data.choices[0].text} \n`
-//     renderTypewriterText(response.data.choices[0].text)
-// }
-
-// function renderTypewriterText(text) {
-//     const newSpeechBubble = document.createElement('div')
-//     newSpeechBubble.classList.add('speech', 'speech-ai', 'blinking-cursor')
-//     chatbotConversation.appendChild(newSpeechBubble)
-//     let i = 0
-//     const interval = setInterval(() => {
-//         newSpeechBubble.textContent += text.slice(i-1, i)
-//         if (text.length === i) {
-//             clearInterval(interval)
-//             newSpeechBubble.classList.remove('blinking-cursor')
-//         }
-//         i++
-//         chatbotConversation.scrollTop = chatbotConversation.scrollHeight
-//     }, 50)
-// }
 const chatbotConversation = document.getElementById('chatbot-conversation')
  
 let conversationStr = ''
@@ -71,7 +16,7 @@ document.addEventListener('submit', (e) => {
 }) 
 
 async function fetchReply(){
-    const url = 'https://https://fine-tuned-chatbot.netlify.app/.netlify/functions/fetchGPT'
+    const url = 'https://https://fine-tuned-chatbot.netlify.app/.netlify/functions/fetchAI'
     
     const response = await fetch(url, {
         method: 'POST',
